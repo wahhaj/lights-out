10f5dc3872e68f471f4baa26799813e49fcee96a
Wahhaj Ali <wahhajj@gmail.com>
2017-04-15 02:55:47 -0400

    Board component now receives an initial board state to start the game

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,47 +1,97 @@
 <template>
-  <div class="board" :style="gridTemplate">
+  <div class="game" v-if="boardState.length">
 
-    <template v-for="(row, r) in board">
-      <template v-for="(col, c) in row">
-        <tile :lit="col" :row="r" :col="c" @click.native="makeMove(r,c)"></tile>
+    <div class="board" :style="gridTemplate">
+      <template v-for="(row, r) in boardState">
+        <template v-for="(col, c) in row">
+          <tile :lit="col" :row="r" :col="c" @click.native="makeMove(r,c)"></tile>
+        </template>
       </template>
-    </template>
+    </div>
+
+    <p v-if="finished">Congratulations!</p>
+    <p>Tiles remaining: {{remaining}}</p>
+    <p>Number of moves: {{moves}}</p>
+
+    <button @click="restart">Restart</button>
 
   </div>
 </template>
 
 <script>
+import Tile from './Tile.vue'
+
 export default {
+
+  components: {
+    Tile
+  },
+
+  props: {
+    initialState: {
+      type: Array,
+      required: true
+    },
+    size: {
+      type: Number,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      boardState: this.cloneInitialState(),
+      moves: 0
+    }
+  },
+
+  computed: {
+    finished: function () {
+      return this.remaining === 0
+    },
+    gridTemplate: function () {
+      return `grid-template: repeat(${this.size}, 1fr) / repeat(${this.size}, 1fr)`
+    },
+    remaining: function () {
+      return [].concat(...this.boardState) // flatten boardState array
+          .filter((lit) => lit).length // count number of `true` values
+    }
+  },
+
   methods: {
+    cloneInitialState: function () {
+      const boardState = []
+      if (this.initialState.length) {
+        this.initialState.forEach((row) => {
+          boardState.push(row.slice())
+        })
+      }
+      return boardState
+    },
     makeMove: function (row, col) {
       this.toggleTile(row, col)
       this.toggleTile(row - 1, col)
       this.toggleTile(row + 1, col)
       this.toggleTile(row, col - 1)
       this.toggleTile(row, col + 1)
+
+      this.moves += 1
+    },
+    restart: function () {
+      this.boardState = this.cloneInitialState()
+      this.moves = 0
     },
     toggleTile: function (row, col) {
       if (row >= 0 && row < this.size && col >= 0 && col < this.size) {
-        this.board[row].splice(col, 1, !this.board[row][col])
+        this.boardState[row].splice(col, 1, !this.boardState[row][col])
       }
     }
   },
-  data () {
-    const size = 6
-    const board = []
-    for (let i = 0; i < size; i++) {
-      board.push(Array(size).fill(false))
-    }
 
-    return {
-      board,
-      size
-    }
-  },
-
-  computed: {
-    gridTemplate: function () {
-      return `grid-template: repeat(${this.size}, 1fr) / repeat(${this.size}, 1fr)`
+  watch: {
+    initialState: function () {
+      this.boardState = this.cloneInitialState()
+      this.moves = 0
     }
   }
 }
