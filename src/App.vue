ba7506a005a4eb32437be1dd8d02dae1294e0e7b
Wahhaj Ali <wahhajj@gmail.com>
2017-04-15 02:40:55 -0400

    Updates app css styles

--- src/App.vue
+++ src/App.vue
@@ -28,42 +28,47 @@ export default {
   $primary: #35495e;
   $accent: #42b983;
   $dark: #2c3e50;
+  $transition: 0.15s ease-in;
 
   body {
     margin: 0;
-  }
-  #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     color: $dark;
+  }
 
+  #app {
     display: grid;
     grid-gap: 1rem;
-    grid-template-areas:
-     "header"
-     "board"
-     "sidebar"
-     "footer";
-
     grid-template-columns: 1fr;
     grid-auto-rows: minmax(100px, auto);
 
-     @media (min-width: 600px) {
-        grid-template-columns: 200px 1fr;
-        grid-template-areas:
-          "header   header"
-          "sidebar  board"
-          "footer   footer";
-     }
+    grid-template-areas:
+     "header"
+     "sidebar"
+     "board"
+     "footer";
+    @media (min-width: 600px) {
+      grid-template-columns: 200px 1fr;
+      grid-template-areas:
+        "header   header"
+        "sidebar  board"
+        "footer   footer";
+    }
+  }
+
+  .header, .footer {
+    display: flex;
+    align-items: center;
+    padding: 1rem 2rem;
+
+    border-color: #eee;
+    border-style: solid;
+    border-width: 0;
   }
 
   .header {
     grid-area: header;
-    display: flex;
-    align-items: center;
-    padding: 1rem 2rem;
-    border-bottom: 1px solid #eee;
+    border-bottom-width: 1px;
 
     img.logo {
       margin-right: 1rem;
@@ -75,18 +80,71 @@ export default {
       font-size: 1.4rem;
     }
   }
+
+  .footer {
+    grid-area: footer;
+    border-top-width: 1px;
+  }
+
   .sidebar {
     grid-area: sidebar;
+    padding: 1rem 1rem;
+
+    ul.links {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+
+      li > a {
+        display: block;
+        font-size: 1.2rem;
+        margin: 0.5rem 0;
+        padding: 0.5rem 1rem;
+        border-radius: 3px;
+
+        transition: background-color 0.15s ease;
+        &:hover {
+          background-color: lighten($accent, 40);
+        }
+      }
+    }
   }
+
   .main {
     grid-area: board;
     justify-self: center;
+    padding: 1rem 1rem;
   }
-  .footer {
-    grid-area: footer;
-    display: flex;
-    align-items: center;
-    padding: 1rem 2rem;
-    border-top: 1px solid #eee;
+
+  a {
+    color: $accent;
+    text-decoration: none;
+  }
+
+  button {
+    background-color: $accent;
+    color: #fff;
+    font-size: 1rem;
+
+    margin: 1rem;
+    padding: 0.5rem 1rem;
+
+    border: none;
+    border-radius: 3px;
+    box-shadow: 0 5px $primary;
+
+    cursor: pointer;
+    outline: none;
+
+    transition: background-color 0.05s ease-in, transform $transition, box-shadow $transition;
+
+    &:hover {
+      background-color: lighten($accent, 20);
+    }
+
+    &:active {
+      box-shadow: 0 2px $primary;
+      transform: translateY(2px);
+    }
   }
 </style>
